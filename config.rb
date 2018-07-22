14b3fe26538c96359b5f62337b7de516ff70e337
Brian Kelly <bkelly@carbonfive.com>
2018-07-22 14:33:10 -0400

    Start providing custom chord/lyric charts on the site

--- config.rb
+++ config.rb
@@ -1,3 +1,6 @@
+require 'tilt'
+require 'tilt/template'
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
@@ -11,4 +14,22 @@ end
 
 set :markdown_engine, :redcarpet
 set :markdown, fenced_code_blocks: true, smartypants: true
+
+class ChordProTemplate < Tilt::Template
+  self.default_mime_type = 'text/html'
+
+  def prepare
+    @output = nil
+  end
+
+  def evaluate(scope, locals, &block)
+    @output ||= Chordpro.html(data)
+  end
+
+  def allows_script?
+    false
+  end
+end
+
+Tilt.register ChordProTemplate, 'crd'
 
