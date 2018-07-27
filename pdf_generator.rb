63d17da4fe539d00e3b176c24bb0d76aa60eeaf8
Brian Kelly <bkelly@carbonfive.com>
2018-07-27 15:58:53 -0400

    Adds a blank page after 'short' songs so all songs spread across facing pages

--- pdf_generator.rb
+++ pdf_generator.rb
@@ -18,6 +18,7 @@ class PdfGenerator < Middleman::Extension
   def after_build(builder)
     FileUtils::mkdir_p 'build/pdfs'
 
+    generate_blank_pdf
     generate_song_pdfs
     generate_songbook_pdf
   end
@@ -48,6 +49,22 @@ class PdfGenerator < Middleman::Extension
     resources << Middleman::Sitemap::Resource.new(@app.sitemap, song_path, song_source)
   end
 
+  def generate_blank_pdf
+    file = File.open('build/blank_page/index.html', "rb")
+    html = file.read
+    kit = PDFKit.new(html,
+                     page_size: 'Letter',
+                     margin_top: 10,
+                     margin_bottom: 10,
+                     margin_left: 10,
+                     margin_right: 10,
+                     print_media_type: true,
+                     dpi: 480
+    )
+    kit.to_file('build/pdfs/blank.pdf')
+  end
+
+
   def generate_song_pdf(song)
     html_path = "build/songs/#{song['chordpro']}/index.html"
     pdf_path = "build/pdfs/#{song['chordpro']}.pdf"
@@ -75,15 +92,22 @@ class PdfGenerator < Middleman::Extension
 
   def generate_songbook_pdf
     songbook_pdf = CombinePDF.new
+    songbook_pdf << CombinePDF.load("#{__dir__}/build/pdfs/blank.pdf")
+
     SONGS.each do |song|
       songbook_pdf << CombinePDF.load("#{__dir__}/build/pdfs/#{song['chordpro']}.pdf")
+      if song['short']
+        songbook_pdf << CombinePDF.load("#{__dir__}/build/pdfs/blank.pdf")
+      end
     end
+
     songbook_pdf.number_pages(
         number_format: '%s',
         location: :bottom_right,
         margin_from_height: 5,
         font_size: 9
     )
+
     songbook_pdf.save("#{__dir__}/build/pdfs/songbook.pdf")
   end
 end
