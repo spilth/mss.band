4cdee5dc3e4f751cbcda3b0f356fb2ce230b3c28
Brian Kelly <polymonic@gmail.com>
2019-05-29 07:01:12 -0400

    wip

--- extensions/chords.rb
+++ extensions/chords.rb
@@ -1,8 +1,12 @@
 class Chords < Middleman::Extension
-  expose_to_template :chord_svg
+  expose_to_template :chord_svg, :tablature_svg
 
   def chord_svg(name, fingerings = nil)
     ChordDiagrams.chord_svg(name, fingerings)
+  end
+
+  def tablature_svg(tablature)
+    Tablature.svg(tablature).render
   end
 end
 
